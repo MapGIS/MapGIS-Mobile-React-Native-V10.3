694872309b37ddee641f48c7bb41ab1151fb75db
fangjinlong <fangjinlong.luck@mapgis.com>
2019-07-12 17:57:30 +0800

    1. MapGIS Mobile ReactNative npm打包编译   2. MapGIS Mobile ReactNative自测

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-/**
- * @format
- */
+import { NativeModules } from 'react-native';
+import MGMapView from "./NativeModule/components/MGSMapViewUI";
+import Environment from './NativeModule/Environment'
+import Dot from './NativeModule/Dot'
+import Feature from './NativeModule/Feature'
+import FeaturePagedResult from './NativeModule/FeaturePagedResult'
+import FeatureQuery from './NativeModule/FeatureQuery'
+import Graphic from './NativeModule/Graphic'
+import GraphicCircle from './NativeModule/GraphicCircle'
+import GraphicImage from "./NativeModule/GraphicImage";
+import GraphicMultiPoint from "./NativeModule/GraphicMultiPoint";
+import GraphicPoint from "./NativeModule/GraphicPoint";
+import GraphicPolygon from "./NativeModule/GraphicPolygon";
+import GraphicPolylin from "./NativeModule/GraphicPolylin";
+import GraphicsOverlay from "./NativeModule/GraphicsOverlay";
+import GraphicsOverlays from "./NativeModule/GraphicsOverlays";
+import GraphicStippleLine from "./NativeModule/GraphicStippleLine";
+import GraphicText from "./NativeModule/GraphicText";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import Image from './NativeModule/Image'
+import Map from './NativeModule/Map'
+import MapLayer from './NativeModule/MapLayer'
+import MapView from './NativeModule/MapView'
+import PointF from './NativeModule/PointF'
+import QueryBound from './NativeModule/QueryBound'
+import Rect from './NativeModule/Rect'
+import VectorLayer from './NativeModule/VectorLayer'
 
-AppRegistry.registerComponent(appName, () => App);
+
+export {
+    MGMapView,
+    Environment,
+    Dot,
+    Feature,
+    FeaturePagedResult,
+    FeatureQuery,
+    Graphic,
+    GraphicCircle,
+    GraphicImage,
+    GraphicMultiPoint,
+    GraphicPoint,
+    GraphicPolygon,
+    GraphicPolylin,
+    GraphicsOverlay,
+    GraphicsOverlays,
+    GraphicStippleLine,
+    GraphicText,
+    Image,
+    Map,
+    MapView,
+    PointF,
+    QueryBound,
+    Rect,
+    VectorLayer,
+};
